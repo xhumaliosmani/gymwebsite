f99d02d952e0bb315b46ba69eb082fd045912bb8
Djumali Osmani <xhumaliosmani@outlook.com>
2023-02-22 22:27:10 +0100

    ready for deploy fixed bugs

--- src/scenes/OurClasses/index.tsx
+++ src/scenes/OurClasses/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { SelectedPage, ClassType } from '@/shared/types'
-import image1 from '@/assets/image1.png'
-import image2 from '@/assets/image2.png'
-import image3 from '@/assets/image3.png'
-import image4 from '@/assets/image4.png'
-import image5 from '@/assets/image5.png'
-import image6 from '@/assets/image6.png'
-import { motion } from 'framer-motion'
-import HText from '@/shared/HText'
-import Class from "./Class"
+import { SelectedPage, ClassType } from "@/shared/types";
+import image1 from "@/assets/image1.png";
+import image2 from "@/assets/image2.png";
+import image3 from "@/assets/image3.png";
+import image4 from "@/assets/image4.png";
+import image5 from "@/assets/image5.png";
+import image6 from "@/assets/image6.png";
+import { motion } from "framer-motion";
+import HText from "@/shared/HText";
+import Class from "./Class";
 
 const classes: Array<ClassType> = [
 
@@ -45,47 +44,51 @@ const classes: Array<ClassType> = [
 ]
 
 type Props = {
-    setSelectedPage: (value: SelectedPage) => void;
-}
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const index = ({setSelectedPage}: Props) => {
+const OurClasses = ({ setSelectedPage }: Props) => {
   return (
-    <section id="ourclasses" className='w-full bg-primary-100 py-40 '>
+    <section id="ourclasses" className="w-full bg-primary-100 py-40">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClasses)}
+      >
         <motion.div
-        onViewportEnter={()=> setSelectedPage(SelectedPage.OurClasses)}>
-            <motion.div className='mx-auto w-5/6'
-             initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true, amount: 0.5 }}
-            transition={{ duration: 0.5 }}
-            variants={{
-              hidden: { opacity: 0, x: -50 },
-              visible: { opacity: 1, x: 0 },
-            }}>
-                <div className='md:w-3/5 '>
-                    <HText>OUR CLASSES</HText>
-                    <p className='py-5'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-optio, eaque rerum! Provident similique accusantium nemo autem.
-                    </p>
-                </div>
-            </motion.div>
-            <div className='mt-10 h-[353px] w-full overflow-x-auto overflow-y-hiden'>
-                <ul className='w-[2800] whitespace-nowrap'>
-                    {classes.map((item: ClassType, index)=> (
-                        <Class 
-                        key={`${item.name}-${index}`}
-                        name={item.name}
-                        description={item.description}
-                        image={item.image}/>
-                    ))}
-                </ul>
-            </div>
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="md:w-3/5">
+            <HText>OUR CLASSES</HText>
+            <p className="py-5">
+              Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+              tellus quam porttitor. Mauris velit euismod elementum arcu neque
+              facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+              enim mattis odio in risus nunc.
+            </p>
+          </div>
         </motion.div>
+        <div className="mt-10 h-[353px] w-full overflow-x-auto overflow-y-hidden">
+          <ul className="w-[2800px] whitespace-nowrap">
+            {classes.map((item: ClassType, index) => (
+              <Class
+                key={`${item.name}-${index}`}
+                name={item.name}
+                description={item.description}
+                image={item.image}
+              />
+            ))}
+          </ul>
+        </div>
+      </motion.div>
     </section>
-  )
-}
+  );
+};
 
-export default index
+export default OurClasses;
